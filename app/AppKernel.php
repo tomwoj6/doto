385ea784d5f9736d9ce7e2d58b876e0bde68ded2
tomwoj6 <tomwoj6@gmail.com>
2016-03-20 18:19:45 +0100

    test

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new UserBundle\UserBundle(),
             new FOS\UserBundle\FOSUserBundle(),
+            new Fp\OpenIdBundle\FpOpenIdBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
